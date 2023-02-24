ed15c7ee77911cd3322a46d0d4bbd3ec01f94f5a
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-02-24 23:12:11 +0000

    Add javascript to hide and revealdescription

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hi");
+console.log("check");
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  let content = button.nextElementSibling;
+  button.addEventListener("click", () => {
+    content.classList.toggle("active");
+    if (button.textContent == "Read more") {
+      button.textContent = "Read less";
+    } else {
+      button.textContent = "Read more";
+    }
+  });
+});
